bd80331ceb775ea208c039c639bfcafcb5df861e
Adesh Dooraree <101601277+ADoor22@users.noreply.github.com>
2024-04-13 16:35:13 +0100

    server edition

--- backend/server.py
+++ backend/server.py
@@ -2,14 +2,27 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def get_request():
-    return "This is a GET request."
+    dataJ = {
+        'message': 'This is a JSON response!'
+    }
+    return jsonify(dataJ)
 
-@app.route('/', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def post_request():
-    data = request.json
-    return jsonify({"message": "Received POST request", "data": data})
+    try:
+        data = request.json 
+        user_private_key = data.get('userPrivateKey')
+        item_amount = data.get('itemAmount')
+        item = data.get('item')
+        item_vendor_public_key = data.get('itemVendorPublicKey')
+        interval = data.get('interval')
+
+
+        return jsonify({"message": "Received POST request", "data": data})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
