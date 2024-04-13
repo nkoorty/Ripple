3c1dbfe59e6c6d1ac426bc269037d2c1d0facc82
acse-am9023 <artemiy.acse@gmail.com>
2024-04-13 20:37:22 +0100

    add

---
+++ RippleX/RippleX/RippleXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleXApp.swift
+//  RippleX
+//
+//  Created by Artemiy Malyshau on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RippleXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
