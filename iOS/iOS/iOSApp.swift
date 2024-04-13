abdb7779b058a473ef22eca1880e41fadab97270
Christian Grinling <grinlingc@gmail.com>
2024-04-13 11:13:37 +0100

    Added iOS app

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Christian Grinling on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
